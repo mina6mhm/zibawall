780915527f4110cebf389633a7b6e9b69b4642f0
mina6mhm <mina47082@gmail.com>
2026-09-04 15:50:43 +0430

    exclude .well-known from auth middleware

--- middleware.ts
+++ middleware.ts
@@ -36,6 +36,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|manifest.json|.*\\.(?:png|jpg|jpeg|svg|ico)$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|manifest.json|\\.well-known|.*\\.(?:png|jpg|jpeg|svg|ico)$).*)',
   ],
 };
